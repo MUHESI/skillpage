987caf924b1514c38bbe4899ba3f212b10824025
MUHESI <mvmmuhei@gmail.com>
2020-09-24 22:32:22 +0200

    Installation de react-router et dom

--- src/App.js
+++ src/App.js
@@ -2,17 +2,23 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import './bootstrap.min.css';
+import Counter from "./components/Counter";
+import  {Route, Switch ,Link, BrowserRouter as Router} from 'raect-router-dom',
 //import 'bootstrap/dist/css/bootstrap.min.css';
 
-import Counter from './composant/counter';
+//import Counter from './composant/counter';
 // { }
 function App() {
   return (
-    <div className="m-3">
-      <Counter title="COunter 1" value={1} image="img/profile3.jpg"/>
-      <Counter title="counter 2" value={1} image="img/profile1.jpg"/>
+<div>
+  <nav>
+    link>
 
-    </div>
+
+
+    </nav>
+
+  </div>
   );
 }
 
