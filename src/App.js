23fa6b7fb5ec46335142bffa5921728d07e290e2
MUHESI <mvmmuhei@gmail.com>
2020-09-24 23:04:13 +0200

    valider l affichage gallery

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import './bootstrap.min.css';
-import Counter from "./components/Counter";
-import  {Route, Switch ,Link, BrowserRouter as Router} from 'raect-router-dom',
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "./bootstrap.min.css";
+//import Counter from "./components/counter";
+import {
+  Counter,
+  Route,
+  Switch,
+  Link,
+  BrowserRouter as Router,
+} from "react-router-dom";
 //import 'bootstrap/dist/css/bootstrap.min.css';
 
-//import Counter from './composant/counter';
 // { }
 function App() {
   return (
-<div>
-  <nav>
-    link>
-
-
-
-    </nav>
-
-  </div>
+    <Router>
+      <nav>
+        <Link to="/home">Home</Link>
+        <Link to="/counter">Counter</Link>
+        <Link to="/about">About</Link>
+        <Link to="/gallery">Gallery</Link>
+      </nav>
+    </Router>
   );
 }
 
