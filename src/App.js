4f4813fb8432503601a7f45c4c770cfa29f9bbfb
MUHESI <mvmmuhei@gmail.com>
2020-09-29 00:08:43 +0200

    files saved

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Profiler } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import "./bootstrap.min.css";
@@ -13,39 +13,9 @@ import { Route, Switch, Link, BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
-      <nav className="navbar navbar-expand navbar-brand m-2">
-        <ul className="navbar-nav">
-          <li>
-            <Link className="nav-link" to="/home">
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link className="nav-link" to="/about">
-              about
-            </Link>
-          </li>
-          <li>
-            <Link className="nav-link" to="/counter">
-              counter
-            </Link>
-          </li>
-          <li>
-            <Link className="nav-link" to="/gallery">
-              gallery
-            </Link>
-          </li>
-        </ul>
-      </nav>
-      <div className="container">
-        <Switch>
-          <Route path="/home"></Route>
-          <Route path="/about" component={Counter}></Route>
-          <Route path="/gallery" component={Counter}></Route>
-        </Switch>
-      </div>
-    </Router>
+    <div className="m-3">
+      <Counter value="Counter :" value={1} image="img/Profiler1.jpg" />
+    </div>
   );
 }
 
--- src/composant/counter.js
+++ src/composant/counter.js
@@ -1,34 +1,54 @@
 import React, { Component } from "react";
 // { }
- //  [] 
-class Counter extends Component { 
-    constructor(props) {   
-        super(props);
-        this.state={
-          counter:1,
-          list:[0] 
-       }
-    }
-    compute=(op)=>{
-        let c =this.state.counter+1;
-        this.setState({
-            counter:c
-        });
-    }
-    render(){ 
-        return(
-            <div className="card">
-                <div className="card-header"><strong>{this.props.title} : { this.state.counter}</strong></div>
-                <div className="ml-auto">
-                    <button onClick= {()=>this.compute("op","+")} className="btn btn-primary m-2" >+</button>
-                    <button onClick= {()=>this.componentDidUpdate("op","-")} className="btn btn-primary m-2" >-</button>
+//  []
 
-                </div>
-                <div className="card-body">
-                    <img src={this.props.image} />  
-                </div>
-            </div>
-        )
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // cest dans le constructeur q un defini le state
+      counter: 1,
+      list: [0],
     };
+  }
+  compute = (op) => {
+    let c = this.state.counter + 1;
+    this.setState({
+      counter: c,
+      list: new Array(c).fill(0),
+    });
+  };
+  // { }
+  render() {
+    return (
+      <div className="card">
+        <div className="card-header">
+          <strong>
+            {this.props.title} : {this.state.counter}
+          </strong>
+        </div>
+        <div className="ml-auto">
+          <button
+            onClick={() => this.compute("op", "+")}
+            className="btn btn-primary m-2"
+          >
+            +
+          </button>
+          <button
+            onClick={() => this.componentDidUpdate("op", "-")}
+            className="btn btn-primary m-2"
+          >
+            -
+          </button>
+        </div>
+        <div className="card-body">
+          {this.state.map((v, index) => (
+            <img width={100} src={this.props.image} />
+          ))}
+          <img width={100} src={this.props.image} />
+        </div>
+      </div>
+    );
+  }
 }
- export default Counter
+export default Counter;
