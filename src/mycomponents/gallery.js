31d65e3cee57c854ca5dba436e57dad15ade71af
MUHESI <mvmmuhei@gmail.com>
2020-12-20 22:36:21 +0200

    save gallery

--- src/mycomponents/gallery.js
+++ src/mycomponents/gallery.js
@@ -1,8 +1,64 @@
 import React, { Component } from "react";
+import axios from "axios";
 
+// { }  []
+
+/*
+listArticles: [
+        {
+          id: 1,
+          title: "Project title1",
+          img: "img/profile3.jpg",
+          author: "Jacques Ndavaro",
+        },
+
+
+        currentPage: 1,
+      pageSize: 10,
+      currentKeyword: "Paris",
+*/
 class Gallery extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hits: [],
+      currentPage: 1,
+      pageSize: 10,
+      currentKeyword: "Paris",
+    };
+  }
+  k;
+  componentDidMount() {
+    this.getHits();
+  }
+
+  getHits() {
+    let url =
+      "https://pixabay.com/api/?key=5832566-81dc7429a63c86e3b707d0429&q=paris";
+
+    axios
+      .get(url)
+      .then((resp) => {
+        this.setState({
+          hits: resp.data.hits,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
   render() {
-    return <div>Hello, I m'a Gallery component !</div>;
+    return (
+      <div>
+        Hello, I m'a Gallery component !
+        <div>
+          {this.state.hits.map((hit, v) => (
+            <img src={hit.webformatURL} />
+          ))}
+        </div>
+      </div>
+    );
   }
 }
 export default Gallery;
